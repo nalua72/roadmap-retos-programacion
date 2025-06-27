107c42f24a743a897c6ea39c30edb9612fcb2080
Jose Rodriguez <nalua72@gmail.com>
2025-06-27 20:02:27 +0200

    Add nalua72.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/nalua72.py
@@ -0,0 +1,18 @@
+# https://www.python.org/
+
+# This is a one line comment
+
+""" This is a
+multiline comment """
+
+variable = 0 # This is a initialized variable to 0
+CONSTANT = 45 # Constant are supposed to be inmutable
+
+v_string = "this a string" # String variable
+v_int = 0 # Int variable
+v_float = 3.5 # Float variable
+v_boolean = True # Boolean variable, the other possible value is False
+
+print("¡Hola PYTHON!")
+
+
